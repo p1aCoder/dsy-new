ec947aac7bbfd2fb8840887bb3c3c195a4614bca
p1aCoder <362958457@qq.com>
2020-10-11 16:03:55 +0800

    Initial commit

---
+++ src/main/java/com/gm/dsy/DsyApplication.java
@@ -0,0 +1,13 @@
+package com.gm.dsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gm/dsy/DsyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm.dsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

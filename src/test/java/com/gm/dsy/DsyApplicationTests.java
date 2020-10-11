28892f5ac1a3a3bf4705c6c1c210ece59765ee8d
p1aCoder <362958457@qq.com>
2020-10-11 19:50:53 +0800

    3

--- src/test/java/com/gm/dsy/DsyApplicationTests.java
+++ src/test/java/com/gm/dsy/DsyApplicationTests.java
@@ -1,6 +1,7 @@
 package com.gm.dsy;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +9,7 @@ class DsyApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("444");
     }
 
 }
